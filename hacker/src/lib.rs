e004b1b19f218ee2b3efb58e9e620f1ce27427e9
michal92299 <chudybyczek2137@gmail.com>
2025-11-18 17:00:30 +0100

    Update lib.rs

--- hacker/src/lib.rs
+++ hacker/src/lib.rs
@@ -2,14 +2,11 @@ mod commands;
 mod game;
 mod help;
 mod utils;
-
 pub use commands::{handle_unpack, handle_system, handle_run};
 pub use game::play_game;
 pub use help::display_ascii;
 pub use utils::{handle_update, run_command_with_spinner};
-
 use clap::Subcommand;
-
 #[derive(Subcommand)]
 pub enum UnpackCommands {
     /// Install add-ons (Wine, BoxBuddy, Winezgui, Gearlever)
@@ -33,13 +30,11 @@ pub enum UnpackCommands {
     /// Install and setup gamescope-session-steam
     GamescopeSessionSteam,
 }
-
 #[derive(Subcommand)]
 pub enum SystemCommands {
     /// Show system logs
     Logs,
 }
-
 #[derive(Subcommand)]
 pub enum RunCommands {
     /// Update the system
@@ -54,5 +49,6 @@ pub enum RunCommands {
     HackerosGameMode,
     /// Update HackerOS
     UpdateHackeros,
+    /// Update wallpapers
+    UpdateWallpapers,
 }
-
