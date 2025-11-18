f9a97d63b0328becd9e1a61fd58dfb17f5f9a5ac
michal92299 <chudybyczek2137@gmail.com>
2025-11-18 17:00:16 +0100

    Update help.rs

--- hacker/src/help.rs
+++ hacker/src/help.rs
@@ -1,6 +1,5 @@
 use colored::*;
 use std::fs;
-
 pub fn display_ascii() {
     match fs::read_to_string("/usr/share/HackerOS/Config-Files/HackerOS-Ascii") {
         Ok(content) => println!("{}", content.bright_cyan().bold().on_black()),
